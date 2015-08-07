767248703a25e7a15f767bac596fef21a9a79363
Zachary Palchick <zpalchick@5amsolutions.com>
2015-08-06 17:10:56 -0700

    Fixes line wrap

--- send.py
+++ send.py
@@ -10,8 +10,8 @@ channel = connection.channel()
 channel.queue_declare(queue='hello')
 
 # Send the message to the queue
-channel.basic_publish(exchange='',
-    routing_key='hello', body='Hello World!')
+channel.basic_publish(
+    exchange='', routing_key='hello', body='Hello World!')
 
 print(" [x] Sent 'Hello World'")
 
