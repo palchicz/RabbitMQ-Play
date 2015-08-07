16865b62e06953496fc41483768d5e1d60e4fec6
Zachary Palchick <zpalchick@5amsolutions.com>
2015-08-07 14:20:25 -0700

    Updates a comment

--- receive.py
+++ receive.py
@@ -19,6 +19,8 @@ def callback(ch, method, properties, body):
 
 
 # Register the callback with Pika
+# no_ack means don't acknowledge rabbit when the task is done.
+# This means if the receiver dies mid-work, the message is lost
 channel.basic_consume(callback, queue='hello', no_ack=True)
 print(' [*] Waiting for messages. To exit, press CTRL+C')
 channel.start_consuming()
